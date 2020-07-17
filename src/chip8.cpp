3b69da0666d38a8e6b86bd5adc49f4cc6b01017f
Jeremy Tieman <johnathon.tieman@gmail.com>
2020-07-17 10:41:14 -0700

    Added log statements for debugging

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,5 +1,6 @@
 #include "chip8.h"
 #include <cstdlib>
+#include <iostream>
 #include <sstream>
 
 namespace CodexMachina
@@ -124,21 +125,31 @@ namespace CodexMachina
     {
       unsigned char newVF = 0x0;
       const auto vx = _V[(opcode & 0x0F00) >> 8];
+      std::cout << "vx: " << vx << "\n";
       const auto vy = _V[(opcode & 0x00F0) >> 4];
+      std::cout << "vy: " << vy << "\n";
       const unsigned char n = (opcode & 0x000F);
+      std::cout << "n: " << n << "\n";
 
       for (unsigned char i = 0; i < n; ++i)
       {
         const auto byte = _memory[_I + i];
+        std::cout << "byte: " << byte << "\n";
 
         for (int j = 0; j < 8; ++j)
         {
           const auto bit = static_cast<unsigned char>((byte >> (7 - j)) & 0x01);
+          std::cout << "bit: " << bit << "\n";
           const auto displayXIndex = (vx + j) % X;
+          std::cout << "displayXIndex: " << displayXIndex << "\n";
           const auto displayYIndex = ((vy + i) % Y) * X;
+          std::cout << "displayYIndex: " << displayYIndex << "\n";
           const auto currentValue = _display[displayXIndex + displayYIndex];
+          std::cout << "currentValue: " << currentValue << "\n";
           const auto newValue = currentValue ^ bit;
+          std::cout << "newValue: " << newValue << "\n";
           if ((currentValue == 1) && (newValue == 0)) newVF = 0x01;
+          std::cout << "newVF: " << newVF << "\n";
           _display[displayXIndex + displayYIndex] = newValue;
         }
       }
